4bff19241c0f90c1d64b3f9f290663e339c31de6
Chris Donnelly <chris.donnelly@coxautoinc.com>
2023-01-18 12:25:51 -0600

    Initial code

---
+++ src/Issues.Inheritdoc/IMyService.cs
@@ -0,0 +1,14 @@
+﻿namespace Issues.Inheritdoc;
+
+/// <summary>
+/// My service interface
+/// </summary>
+public interface IMyService
+{
+    /// <summary>
+    /// Get a string value
+    /// </summary>
+    /// <param name="key">The key</param>
+    /// <returns>The value</returns>
+    string GetStringValue(string key);
+}
---
+++ src/Issues.Inheritdoc/MyService.cs
@@ -0,0 +1,7 @@
+﻿namespace Issues.Inheritdoc;
+
+/// <inheritdoc />
+public class MyService : IMyService
+{
+    public string GetStringValue(string key) => $"{key}itlan";
+}
